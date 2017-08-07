8c13819339ad136ef50f87f2fdf2e9a2a0e78b8b
Khamit Tukenbaev <tukenbaev.khamit@gmail.com>
2017-08-07 12:34:08 +0600

    initial

--- src/kz/platonus/task2/Main.java
+++ src/kz/platonus/task2/Main.java
@@ -15,7 +15,9 @@ public class Main {
     private static final String CHARSET = "UTF-16";
 
     public static void main(String[] args) throws IOException {
+
         Map<Integer,List<String>> messages = FileReader.readFile(INPUT_FILE_NAME);
         FileWriter.sortAndWriteToFile(messages,OUTPUT_FILE_NAME,CHARSET);
+
     }
 }
